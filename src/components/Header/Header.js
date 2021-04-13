98a70c9d5c857f38abec8d4efe60b9aa157d11d5
akash elhance <akashelhance@gmail.com>
2021-04-14 02:14:54 +0530

    components added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+        <nav className="headerMenu">
+            <a href="#">Home</a>
+            <a href="#">About Us</a>
+            <a href="#">Contact Us</a>
+        </nav>
+        <div>
+            socila Media links
+        </div>
+    </header>
+    )
+}
+
+export default Header
---
+++ src/components/Logo/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = (props) => {
+    return (
+        <div className="logo">
+        <a href="#">Rizwan Khan</a>
+    </div>
+    )
+}
+
+export default index
